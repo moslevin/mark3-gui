356c46024df903b9008c80c681dd2b6aa9d84b83
m0slevin <funk_dev@hotmail.com>
2018-02-16 21:29:11 -0500

    initial commit

---
+++ graphics/public/fontport.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#if defined AVR
+#include <avr/pgmspace.h>
+
+#define FONT_STORAGE_TYPE unsigned char
+#define FONT_ATTRIBUTE_TYPE PROGMEM
+
+#define Font_ReadByte(x, y) pgm_read_byte((char*)(&y[x]))
+
+#else
+#define FONT_STORAGE_TYPE unsigned char
+#define FONT_ATTRIBUTE_TYPE
+
+#define Font_ReadByte(x, y) (y[x])
+
+#endif
